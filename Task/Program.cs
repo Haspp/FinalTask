1265ed859a2bcdd5043db1988bb1c93932e56beb
Evgenii <evgensolon@icloud.com>
2022-08-09 23:32:09 +0300

    FinalTask Complete

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] StartArray;
+
+Console.WriteLine("Введите массив строк через проблем: ");
+string userString = Console.ReadLine();
+
+StartArray = userString.Split(' ');
+string[] FinalArray = new string[StartArray.Length];
+
+void SearchFinalArray(string[] StartArray, string[] FinalArray)
+{
+    int count = 0;
+    for (int i = 0; i < StartArray.Length; i++)
+    {
+    if(StartArray[i].Length <= 3)
+        {
+        FinalArray[count] = StartArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SearchFinalArray(StartArray, FinalArray);
+PrintArray(FinalArray);
